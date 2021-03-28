47b6f13bdaed477e298470740df5d0c0db4a0dfc
천수빈 <suebin710@gmail.com>
2021-03-28 22:03:01 +0900

    add secondActivity (temporary)
    로그인 완료 후 사용자 정보를 얻기 위한 임시 화면 생성

---
+++ DDUBUCK/app/src/main/java/com/example/ddubuck/SecondActivity.kt
@@ -0,0 +1,54 @@
+package com.example.ddubuck
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.kakao.sdk.user.UserApiClient
+import org.w3c.dom.Text
+
+class SecondActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast", "SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+
+        val logout : Button = findViewById(R.id.kakao_logout_button)
+        val unlink : Button = findViewById(R.id.kakao_unlink_button)
+        val id : TextView = findViewById(R.id.id)
+        var nickname: TextView = findViewById(R.id.nickname)
+        logout.setOnClickListener {
+            UserApiClient.instance.logout { error ->
+                if (error != null) {
+                    Toast.makeText(this, "로그아웃 실패 $error", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "로그아웃 성공", Toast.LENGTH_SHORT).show()
+                }
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP))
+            }
+        }
+
+        unlink.setOnClickListener {
+            UserApiClient.instance.unlink { error ->
+                if (error != null) {
+                    Toast.makeText(this, "회원 탈퇴 실패 $error", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "회원 탈퇴 성공", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP))
+                }
+            }
+        }
+
+        UserApiClient.instance.me { user, error ->
+            id.text = "회원번호: ${user?.id}"
+            nickname.text = "닉네임: ${user?.kakaoAccount?.profile?.nickname}"
+        }
+    }
+}
