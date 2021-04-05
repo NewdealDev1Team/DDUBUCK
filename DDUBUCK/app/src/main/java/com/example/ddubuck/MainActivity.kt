b0f58201c2944a0ef54839d20db61980c631bdd0
천수빈 <suebin710@gmail.com>
2021-04-05 12:18:58 +0900

    fix conflicts with easy login branch

--- DDUBUCK/app/src/main/java/com/example/ddubuck/MainActivity.kt
+++ DDUBUCK/app/src/main/java/com/example/ddubuck/MainActivity.kt
@@ -1,20 +1,11 @@
 package com.example.ddubuck
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val changeScreen : Button = findViewById(R.id.changeScreen)
 
-        changeScreen.setOnClickListener {
-            val intent = Intent(this, MapFragmentActivity::class.java)
-
-            startActivity(intent)
-        }
     }
 }
