8cad4211a1f6180a6c026a694630fdcf99cad876
천수빈 <suebin710@gmail.com>
2021-04-07 13:55:41 +0900

    add func getCurrentDust

--- DDUBUCK/app/src/main/java/com/example/ddubuck/weather/WeatherActivity.kt
+++ DDUBUCK/app/src/main/java/com/example/ddubuck/weather/WeatherActivity.kt
@@ -5,27 +5,22 @@ import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ddubuck.R
-import com.google.gson.JsonObject
-import org.json.JSONObject
-import org.w3c.dom.Text
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class WeatherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
-        getCurrentWeather()
-
+        //getCurrentWeather()
+        getCurrentDust()
     }
 
     fun getCurrentWeather() {
         val tv: TextView = findViewById(R.id.tv)
-        var res: Call<WeatherResponse> = WeatherClient
+        val res: Call<WeatherResponse> = WeatherClient
                 .getInstance()
                 .buildRetrofit()
                 .getCurrentWeather("37.563598" ,"126.909227", getString(R.string.OPEN_WEATHER_MAP_KEY))
@@ -38,23 +33,51 @@ class WeatherActivity : AppCompatActivity() {
 
             override fun onResponse(call: Call<WeatherResponse>, response: Response<WeatherResponse>) {
                 val weatherResponse = response.body()
-                var cTemp =  weatherResponse!!.main!!.temp - 273.15  //켈빈을 섭씨로 변환
-                var minTemp = weatherResponse!!.main!!.temp_min - 273.15
-                var maxTemp = weatherResponse!!.main!!.temp_max - 273.15
+                val cTemp =  weatherResponse!!.main!!.temp - 273.15  //켈빈을 섭씨로 변환
+                val minTemp = weatherResponse.main!!.temp_min - 273.15
+                val maxTemp = weatherResponse.main!!.temp_max - 273.15
                 val stringBuilder =
-                        "지역: " + weatherResponse!!.sys!!.country + "\n" +
+                        "지역: " + weatherResponse.sys!!.country + "\n" +
                                 "현재기온: " + cTemp + "\n" +
                                 "최저기온: " + minTemp + "\n" +
                                 "최고기온: " + maxTemp + "\n" +
-                                "풍속: " + weatherResponse!!.wind!!.speed+ "\n" +
-                                "일출시간: " + weatherResponse!!.sys!!.sunrise + "\n" +
-                                "일몰시간: " + weatherResponse!!.sys!!.sunset + "\n"+
-                                "아이콘: " + weatherResponse!!.weather!!.get(0).icon + "\n"
+                                "풍속: " + weatherResponse.wind!!.speed+ "\n" +
+                                "일출시간: " + weatherResponse.sys!!.sunrise + "\n" +
+                                "일몰시간: " + weatherResponse.sys!!.sunset + "\n"+
+                                "아이콘: " + weatherResponse.weather.get(0).icon + "\n"
 
-                Log.d("TAG" , "Success :: $response")
                 tv.text = stringBuilder
-
             }
         })
+    }
+
+    fun getCurrentDust() {
+        val tv: TextView = findViewById(R.id.tv)
+        val res: Call<Dust> = DustClient
+                .getInstance()
+                .buildRetrofit()
+                .getCurrentDust("마포구", "DAILY", 1, 1, "json"
+                        , "WUS/HlSHsC8A/VZZlz1//4eSJiXcoh5gfR2EsoqdYGjhybgzun09KJKWZz+slJ85LzMZIIahT9UgeveNhce/yw==")
+
+
+        res.enqueue(object: Callback<Dust> {
+
+            override fun onFailure(call: Call<Dust>, t: Throwable) {
+                Log.d("TAG", "Failure : ${t.message.toString()}")
+            }
+
+            override fun onResponse(call: Call<Dust>, response: Response<Dust>) {
+                val dustResponse = response.body()?.response
+
+                var dustInfo =
+                        "통합대기환경수치 : " + dustResponse?.body!!.items[0].khaiValue + "\n" +
+                                "통합대기환경지수 : " + dustResponse.body.items[0].khaiGrade
+
+                Log.d("TAG" , "Success :: ${dustInfo}")
+                tv.text = dustInfo
+            }
+        })
+
+
     }
 }
