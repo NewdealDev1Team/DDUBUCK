b7eff28bacbb6bd544e5cdef7ee48b566c760c12
천수빈 <suebin710@gmail.com>
2021-04-08 22:17:30 +0900

    remove @serializedName

--- DDUBUCK/app/src/main/java/com/example/ddubuck/weather/WeatherInfo.kt
+++ DDUBUCK/app/src/main/java/com/example/ddubuck/weather/WeatherInfo.kt
@@ -2,46 +2,36 @@ package com.example.ddubuck.weather
 
 import com.google.gson.annotations.SerializedName
 
-class WeatherResponse(){
-    @SerializedName("weather") var weather = ArrayList<Weather>()
-    @SerializedName("main") var main: Main? = null
-    @SerializedName("wind") var wind : Wind? = null
-    @SerializedName("sys") var sys: Sys? = null
+class WeatherResponse() {
+
+    var weather = ArrayList<Weather>()
+    var main: Main? = null
+    var wind: Wind? = null
+    var sys: Sys? = null
 }
 
 class Weather {
-    @SerializedName("id") var id: Int = 0
-    @SerializedName("main") var main : String? = null
-    @SerializedName("description") var description: String? = null
-    @SerializedName("icon") var icon : String? = null
+    var id: Int = 0
+    var main: String? = null
+    var description: String? = null
+    var icon: String? = null
 }
 
 class Main {
-    @SerializedName("temp")
     var temp: Float = 0.toFloat()
-    @SerializedName("humidity")
     var humidity: Float = 0.toFloat()
-    @SerializedName("pressure")
     var pressure: Float = 0.toFloat()
-    @SerializedName("temp_min")
     var temp_min: Float = 0.toFloat()
-    @SerializedName("temp_max")
     var temp_max: Float = 0.toFloat()
-
 }
 
 class Wind {
-    @SerializedName("speed")
     var speed: Float = 0.toFloat()
-    @SerializedName("deg")
     var deg: Float = 0.toFloat()
 }
 
 class Sys {
-    @SerializedName("country")
     var country: String? = null
-    @SerializedName("sunrise")
     var sunrise: Long = 0
-    @SerializedName("sunset")
     var sunset: Long = 0
 }
