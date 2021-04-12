4835c6ba6e28a1e72e231f863fd8877b2ef15564
천수빈 <suebin710@gmail.com>
2021-04-12 16:25:29 +0900

    add Weather Items data column
    데이터 클래스에 빠진 항목 추가

--- DDUBUCK/app/src/main/java/com/example/ddubuck/weather/Dust.kt
+++ DDUBUCK/app/src/main/java/com/example/ddubuck/weather/Dust.kt
@@ -10,28 +10,32 @@ class DustResponse {
 }
 
 class Body {
-    var toalCount: Int = 0
+    var totalCount: Int = 0
     var items = ArrayList<Items>()
     var pageNo: Int = 0
     var numOfRows: Int = 0
 }
 
 class Items {
+
     var so2Grade: String? = null
     var coFlag: String? = null
     var khaiValue: String? = null
     var so2Value: String? = null
     var coValue: String? = null
+
     var pm10Flag: String? = null
     var pm10Value: String? = null
+    var o3Grade: String? = null
     var khaiGrade: String? = null
     var no2Flag: String? = null
+
     var no2Grade: String? = null
     var o3Flag: String? = null
     var so2Flag: String? = null
-
     var dataTime: String? = null
     var coGrade: String? = null
+
     var no2Value: String? = null
     var pm10Grade: String? = null
     var o3Value: String? = null
