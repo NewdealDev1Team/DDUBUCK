8c6278f7a4cca895bb80dadd965d0e0a4b1456a5
zeratulspc <zeratulspc@gmail.com>
2021-04-12 17:04:06 +0900

    add bottom sheet detail

--- DDUBUCK/app/src/main/java/com/example/ddubuck/home/HomeRvAdapter.kt
+++ DDUBUCK/app/src/main/java/com/example/ddubuck/home/HomeRvAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.ddubuck.home
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,18 +7,19 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ddubuck.R
+import com.example.ddubuck.home.bottomSheet.BottomSheetSelectFragment
 
-class HomeRvAdapter(private val context: Context,
-                    private val itemList: ArrayList<CourseItem>,
-                    private val onClick:()->Unit):
+class HomeRvAdapter(private val itemList: ArrayList<CourseItem>,
+                    private val callback: BottomSheetSelectFragment.OnCourseSelectedListener,):
         RecyclerView.Adapter<Holder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val view = LayoutInflater.from(context).inflate(R.layout.bottom_sheet_select_item, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.bottom_sheet_select_item, parent, false)
         return Holder(view)
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.bind(itemList[position], context){onClick}
+        holder.bind(itemList[position], callback)
     }
 
     override fun getItemCount(): Int {
@@ -33,14 +33,18 @@ class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!)  {
     private val picture = itemView?.findViewById<ImageView>(R.id.sheet_item_picture)
 
     //https://blog.yena.io/studynote/2017/12/06/Android-Kotlin-RecyclerView1.html
-    fun bind(i: CourseItem, context: Context,onClick:()->Unit) {
+    fun bind(i: CourseItem,callback : BottomSheetSelectFragment.OnCourseSelectedListener) {
         if(i.isFreeWalk) {
-            itemView.setOnClickListener{println("자유산책")}
+            itemView.setOnClickListener{callback.onCourseSelected(CourseItem(
+                true,
+                "자유산책",
+                "자유산책입니다",
+                WalkRecord(listOf(), listOf(), listOf(), 1, 1, 1.0)),)}
             title?.text = "자유산책"
             body?.text = "나만의 자유로운 산책,\n즐길 준비 되었나요?"
             picture?.setImageResource(R.mipmap.ic_launcher)
         } else {
-            itemView.setOnClickListener{println(i.walkRecord)}
+            itemView.setOnClickListener{callback.onCourseSelected(i)}
             title?.text = "코스산책"
             body?.text = "걸음수 : ${i.walkRecord.stepCount}\n거리 : ${i.walkRecord.distance}\n시간 : ${i.walkRecord.walkTime}"
             picture?.setImageResource(R.mipmap.ic_launcher)
