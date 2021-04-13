6381772fc4068722d6df7c7b3f3ad771b1b05175
user <zeratulspc@gmail.com>
2021-04-13 11:53:16 +0900

    add free walk button listener

--- DDUBUCK/app/src/main/java/com/example/ddubuck/home/bottomSheet/BottomSheetFreeDetailFragment.kt
+++ DDUBUCK/app/src/main/java/com/example/ddubuck/home/bottomSheet/BottomSheetFreeDetailFragment.kt
@@ -5,8 +5,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.ddubuck.R
 import com.example.ddubuck.home.CourseItem
@@ -19,6 +21,10 @@ class BottomSheetFreeDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val rootView  = inflater.inflate(R.layout.bottom_sheet_detail_free,container, false)
+        val startButton : Button = rootView.findViewById(R.id.start_button_free)
+        startButton.setOnClickListener{
+            Toast.makeText(context, "foo", Toast.LENGTH_SHORT).show()
+        }
         return rootView
     }
 
