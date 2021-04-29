c483363d600839e1bae7fc817f59c800155d7cff
zeratulspc <zeratulspc@gmail.com>
2021-04-29 14:58:24 +0900

    fix conflict

--- DDUBUCK/app/src/main/java/com/example/ddubuck/data/publicdata/RetrofitClient.kt
+++ DDUBUCK/app/src/main/java/com/example/ddubuck/data/publicdata/RetrofitClient.kt
@@ -1,21 +1,19 @@
 package com.example.ddubuck.data.publicdata
 
-import com.example.ddubuck.data.Api
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient{
-
-    private const val BASE_URL = "http://3.37.6.181:3000/"
-
-    val instance: Api by lazy {
-        val retrofit = Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-
-        retrofit.create(Api::class.java)
-    }
+//
+//    private const val BASE_URL = "http://3.37.6.181:3000/"
+//
+//    val instance: Api by lazy {
+//        val retrofit = Retrofit.Builder()
+//                .baseUrl(BASE_URL)
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .build()
+//        retrofit.create(Api::class.java)
+//    }
 }
 
 
--- DDUBUCK/app/src/main/java/com/example/ddubuck/ui/challenge/ChallengeFragment.kt
+++ DDUBUCK/app/src/main/java/com/example/ddubuck/ui/challenge/ChallengeFragment.kt
@@ -24,17 +24,17 @@ class ChallengeFragment : Fragment() {
         challengeViewModel =
                 ViewModelProvider(this).get(ChallengeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_challenge, container, false)
-        val textView: TextView = root.findViewById(R.id.text_challenge)
-        challengeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        val frag = BottomSheetFreeDetailFragment()
-        textView.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                    .replace(R.id.challenge_container, frag, null)
-                    .addToBackStack(MainActivity.CHALLENGE_TAG)
-                    .commit()
-        }
+//        val textView: TextView = root.findViewById(R.id.text_challenge)
+//        challengeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+//        val frag = BottomSheetFreeDetailFragment()
+//        textView.setOnClickListener {
+//            parentFragmentManager.beginTransaction()
+//                    .replace(R.id.challenge_container, frag, null)
+//                    .addToBackStack(MainActivity.CHALLENGE_TAG)
+//                    .commit()
+//        }
         return root
     }
 }
