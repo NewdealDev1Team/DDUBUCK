229270fa9f5eeda87113364ca18b8bdc570e2317
exception <suebin710@gmail.com>
2021-04-30 14:36:23 +0900

    add mypage button ripple effect and onclick listener

--- DDUBUCK/app/src/main/java/com/example/ddubuck/ui/mypage/MyPageFragment.kt
+++ DDUBUCK/app/src/main/java/com/example/ddubuck/ui/mypage/MyPageFragment.kt
@@ -12,6 +12,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
@@ -23,15 +25,12 @@ import com.example.ddubuck.ui.mypage.mywalk.CaloriesFragment
 import com.example.ddubuck.ui.mypage.mywalk.CoseClearFragment
 import com.example.ddubuck.ui.mypage.mywalk.WalkTimeFragment
 import com.example.ddubuck.databinding.FragmentMypageBinding
+import com.example.ddubuck.ui.home.HomeMapFragment
 import com.example.ddubuck.weather.Main
 import kotlinx.android.synthetic.main.fragment_mypage.*
 
 
 class MyPageFragment : Fragment() {
-
-//     var walktimFm = WalkTimeFragment()
-//     var coseClearFm = CoseClearFragment()
-//     var caloriesfm = CaloriesFragment()
 
     private lateinit var myPageViewModel: MyPageViewModel
     private lateinit var myPageBinding: FragmentMypageBinding
@@ -40,9 +39,30 @@ class MyPageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_mypage, container, false)
+        val myPageView = inflater.inflate(R.layout.fragment_mypage, container, false)
 
-        return rootView
+        val profileImage: ImageView = myPageView.findViewById(R.id.profile_image)
+        val walkingTimeButton: ConstraintLayout = myPageView.findViewById(R.id.walking_time_button)
+        val courseClearButton: ConstraintLayout = myPageView.findViewById(R.id.course_complete_button)
+        val calorieButton: ConstraintLayout = myPageView.findViewById(R.id.calorie_button)
+
+        // 산책 시간 버튼 onClickListener
+        walkingTimeButton.setOnClickListener {
+
+        }
+
+        // 코스 완주 버튼 onClickListener
+        courseClearButton.setOnClickListener {
+
+        }
+
+        // 칼로리 버튼 onClickListener
+        calorieButton.setOnClickListener {
+
+        }
+
+
+        return myPageView
     }
 }
 
