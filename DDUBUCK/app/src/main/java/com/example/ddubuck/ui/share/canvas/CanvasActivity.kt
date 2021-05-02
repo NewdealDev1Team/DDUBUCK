aad8535407bc05087ec7773c0c1b7f44650ee599
zeratulspc <zeratulspc@gmail.com>
2021-05-02 21:43:45 +0900

    add auto crop & centralize

--- DDUBUCK/app/src/main/java/com/example/ddubuck/ui/share/canvas/CanvasActivity.kt
+++ DDUBUCK/app/src/main/java/com/example/ddubuck/ui/share/canvas/CanvasActivity.kt
@@ -8,6 +8,7 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Rect
+import android.media.ThumbnailUtils
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
