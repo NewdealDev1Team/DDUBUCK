5e2210a17961b53c39e3f05ea0671a544149957a
zeratulspc <zeratulspc@gmail.com>
2021-05-06 17:27:48 +0900

    fix marker obejct

--- DDUBUCK/app/src/main/java/com/example/ddubuck/data/publicdata/Cafe.kt
+++ DDUBUCK/app/src/main/java/com/example/ddubuck/data/publicdata/Cafe.kt
@@ -1,6 +1,8 @@
 package com.example.ddubuck.data.publicdata
 
 data class Cafe(
+    val name : String,
+    val address : String,
     val x: Double,
     val y: Double
 )
--- DDUBUCK/app/src/main/java/com/example/ddubuck/data/publicdata/CarFreeRoad.kt
+++ DDUBUCK/app/src/main/java/com/example/ddubuck/data/publicdata/CarFreeRoad.kt
@@ -1,6 +1,7 @@
 package com.example.ddubuck.data.publicdata
 
 data class CarFreeRoad(
+
     val x: Double,
     val y: Double
 )
