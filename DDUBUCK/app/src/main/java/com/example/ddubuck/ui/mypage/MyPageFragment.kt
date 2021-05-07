acbd4468434a329753619a77e8fe751dc9bd3955
eunyeong1328 <eunyeong1328@daum.net>
2021-05-08 00:43:03 +0900

    show records on mypage

--- DDUBUCK/app/src/main/java/com/example/ddubuck/ui/mypage/MyPageFragment.kt
+++ DDUBUCK/app/src/main/java/com/example/ddubuck/ui/mypage/MyPageFragment.kt
@@ -12,6 +12,8 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.*
 import com.example.ddubuck.MainActivity
 import com.example.ddubuck.R
+import com.example.ddubuck.data.mypagechart.RetrofitChart
+import com.example.ddubuck.data.mypagechart.chartData
 import com.example.ddubuck.databinding.FragmentMypageBinding
 import com.example.ddubuck.login.UserService
 import com.example.ddubuck.login.UserValidationInfo
@@ -72,6 +74,32 @@ class MyPageFragment : Fragment() {
             toEditInfoPage()
 
         }
+        //나의 산책 기록
+        RetrofitChart.instance.getRestsMypage().enqueue(object : Callback<chartData> {
+            override fun onResponse(call: Call<chartData>, response: Response<chartData>) {
+                if (response.isSuccessful) {
+                    Log.d("text", "연결성공")
+                    var timeRecordt6 = response.body()?.weekStat?.get(6)?.walkTime?.toInt()
+                    val walkingTimeButtonRecordFormat : Int = timeRecordt6!!.toInt()
+                    val walkingTimeButtonRecord : TextView = myPageView.findViewById(R.id.walking_time_button_record)
+                    walkingTimeButtonRecord.setText(timeRecordt6.toString())
+
+                    var courseRecord6 = response.body()?.weekStat?.get(6)?.completedCount?.toInt()
+                    val courseEndButtonRecordFormat : Int = courseRecord6!!.toInt()
+                    val courseEndButtonRecord : TextView = myPageView.findViewById(R.id.course_end_button_record)
+                    courseEndButtonRecord.setText(courseRecord6.toString())
+
+                    var calorieRecord6 = response.body()?.weekStat?.get(6)?.calorie?.toInt()
+                    val walkingtimeButtonRecordFormat : Int = calorieRecord6!!.toInt()
+                    val calorieButtonRecord : TextView = myPageView.findViewById(R.id.calorie_button_record)
+                    calorieButtonRecord.setText(calorieRecord6.toString())
+                }
+            }
+
+            override fun onFailure(call: Call<chartData>, t: Throwable) {
+                Log.d("error", t.message.toString())
+            }
+        })
 
         // 산책 시간 버튼 onClickListener
         walkingTimeButton.setOnClickListener {
