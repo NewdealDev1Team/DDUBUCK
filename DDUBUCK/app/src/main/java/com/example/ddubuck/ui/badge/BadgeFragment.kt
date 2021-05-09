37c965a9e4136c1bc534c8387e6c3e29c3f05d32
eunyeong1328 <eunyeong1328@daum.net>
2021-05-09 10:37:29 +0900

    record exposure from mywalk in mypage

--- DDUBUCK/app/src/main/java/com/example/ddubuck/ui/badge/BadgeFragment.kt
+++ DDUBUCK/app/src/main/java/com/example/ddubuck/ui/badge/BadgeFragment.kt
@@ -9,6 +9,9 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.ddubuck.R
+import com.example.ddubuck.databinding.ActivityMainBinding
+import com.example.ddubuck.ui.home.HomeFragment
+import com.example.ddubuck.ui.home.HomeMapFragment
 
 class BadgeFragment : Fragment() {
 
@@ -19,13 +22,19 @@ class BadgeFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        badgeViewModel =
-                ViewModelProvider(this).get(BadgeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_badge, container, false)
+//        badgeViewModel =
+//                ViewModelProvider(this).get(BadgeViewModel::class.java)
+//
 ////        val textView: TextView = root.findViewById(R.id.text_badge)
 //        badgeViewModel.text.observe(viewLifecycleOwner, Observer {
 //            textView.text = it
 //        })
+        //        homeMapFragment = HomeMapFragment(fm, owner)
+//        fm.beginTransaction().add(R.id.home_map_container, homeMapFragment,
+//            HomeFragment.BOTTOM_SHEET_CONTAINER_TAG).commit()
+        val root = inflater.inflate(R.layout.fragment_badge, container, false)
+
+
         return root
     }
 }
