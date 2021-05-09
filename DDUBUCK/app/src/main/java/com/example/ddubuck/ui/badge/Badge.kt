8283eb69aa2119719c234ca4f64a81c65226de6b
eunyeong1328 <eunyeong1328@daum.net>
2021-05-09 14:06:48 +0900

    add badge layout

---
+++ DDUBUCK/app/src/main/java/com/example/ddubuck/ui/badge/Badge.kt
@@ -0,0 +1,26 @@
+package com.example.ddubuck.ui.badge
+
+import android.widget.LinearLayout
+
+//data class Badge(
+//    var title: String,
+//    var bottomTitle : String,
+//    var oneLine: LinearLayout,
+//)
+
+data class Badge(
+    var title: String,
+    var bottomTitle : String,
+    var oneLineImage1: Int,
+    var oneLineImage2: Int,
+    var oneLineImage3: Int,
+    var twoLineText1: String,
+    var twoLineText2: String,
+    var twoLineText3: String,
+    var threeLineImage1: Int,
+    var threeLineImage2: Int,
+    var threeLineImage3: Int,
+    var fourLineText1: String,
+    var fourLineText2: String,
+    var fourLineText3: String,
+)
