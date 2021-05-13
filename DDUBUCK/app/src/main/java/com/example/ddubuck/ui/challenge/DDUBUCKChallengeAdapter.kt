59029af3afe7f8d38ceac668f80c8d3ac7174220
exception <suebin710@gmail.com>
2021-05-13 15:24:25 +0900

    add item click listener

--- DDUBUCK/app/src/main/java/com/example/ddubuck/ui/challenge/DDUBUCKChallengeAdapter.kt
+++ DDUBUCK/app/src/main/java/com/example/ddubuck/ui/challenge/DDUBUCKChallengeAdapter.kt
@@ -1,15 +1,29 @@
 package com.example.ddubuck.ui.challenge
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
+import com.example.ddubuck.MainActivity
 import com.example.ddubuck.R
 import kotlinx.android.synthetic.main.challenge_card_layout.view.*
 
-class DDUBUCKChallengeAdapter() : RecyclerView.Adapter<DDUBUCKChallengeAdapter.ChallengeViewHolder>() {
+class DDUBUCKChallengeAdapter() :
+    RecyclerView.Adapter<DDUBUCKChallengeAdapter.ChallengeViewHolder>() {
+
+    interface ItemClickListener {
+        fun onClick(view: View, position: Int)
+    }
+
+    private lateinit var itemClickListner: ItemClickListener
+
+    fun setItemClickListener(itemClickListener: ItemClickListener) {
+        this.itemClickListner = itemClickListener
+    }
 
     val ddubuckChallengeImages = intArrayOf(
         R.drawable.ic_cumulative_distance,
@@ -23,11 +37,12 @@ class DDUBUCKChallengeAdapter() : RecyclerView.Adapter<DDUBUCKChallengeAdapter.C
         "코스완료"
     )
 
-    val ddubuckChallengeText = arrayOf (
+    val ddubuckChallengeText = arrayOf(
         "내가 걸어온 만큼!",
         "과연 오늘은 몇 보?",
         "코스 클리어하는 재미!"
-     )
+    )
+
 
     class ChallengeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
@@ -38,13 +53,19 @@ class DDUBUCKChallengeAdapter() : RecyclerView.Adapter<DDUBUCKChallengeAdapter.C
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, position: Int): ChallengeViewHolder {
-        val challengeView: View = LayoutInflater.from(viewGroup.context).inflate(R.layout.challenge_card_layout, viewGroup, false)
+        val challengeView: View = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.challenge_card_layout, viewGroup, false)
 
         return ChallengeViewHolder(challengeView)
     }
 
     override fun onBindViewHolder(holder: ChallengeViewHolder, position: Int) {
         holder.challengeItemImage.setImageResource(ddubuckChallengeImages[position])
+
+        holder.itemView.setOnClickListener {
+            itemClickListner.onClick(it, position)
+        }
+
         holder.challengeItemTitle.text = ddubuckChallengeTitles[position]
         holder.challengeItemText.text = ddubuckChallengeText[position]
     }
@@ -52,4 +73,6 @@ class DDUBUCKChallengeAdapter() : RecyclerView.Adapter<DDUBUCKChallengeAdapter.C
     override fun getItemCount(): Int {
         return ddubuckChallengeImages.size
     }
+
+
 }
