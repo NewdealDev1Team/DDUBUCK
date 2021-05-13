aa05be9b02314e745a4d5f420065b05ffaa7cb84
exception <suebin710@gmail.com>
2021-05-13 15:26:42 +0900

    add fragment change logic

--- DDUBUCK/app/src/main/java/com/example/ddubuck/ui/challenge/ChallengeFragment.kt
+++ DDUBUCK/app/src/main/java/com/example/ddubuck/ui/challenge/ChallengeFragment.kt
@@ -1,20 +1,26 @@
 package com.example.ddubuck.ui.challenge
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.ddubuck.MainActivity
 import com.example.ddubuck.R
+import com.example.ddubuck.ui.challenge.detail.ChallengeDetailDistanceFragment
 
 class ChallengeFragment : Fragment() {
 
     private lateinit var challengeViewModel: ChallengeViewModel
+    private lateinit var challengeDeetailFragment: ChallengeDetailDistanceFragment
 
     @SuppressLint("SetTextI18n")
     override fun onCreateView(
@@ -22,25 +28,37 @@ class ChallengeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
+
+        challengeDeetailFragment = ChallengeDetailDistanceFragment()
+
+        var list = arrayOf(
+            challengeDeetailFragment,
+            challengeDeetailFragment
+        )
+
         challengeViewModel = ViewModelProvider(this).get(ChallengeViewModel::class.java)
         val challengeView = inflater.inflate(R.layout.fragment_challenge, container, false)
 
         val challengeLayoutManager = GridLayoutManager(challengeView.context, 2)
-        val challengeRecyclerView: RecyclerView = challengeView.findViewById(R.id.challenge_recyclerView)
+        val challengeRecyclerView: RecyclerView =
+            challengeView.findViewById(R.id.challenge_recyclerView)
         challengeRecyclerView.isNestedScrollingEnabled = false
 
-        val ddubuckChallengeTitle: TextView = challengeView.findViewById(R.id.ddubuck_challenge_title)
+        val ddubuckChallengeTitle: TextView =
+            challengeView.findViewById(R.id.ddubuck_challenge_title)
         val ddubuckChallengeText: TextView = challengeView.findViewById(R.id.ddubuck_challenge_text)
 
         val hiddenChallengeLayoutManager = GridLayoutManager(challengeView.context, 2)
-        val hiddenChallengeRecyclerView: RecyclerView = challengeView.findViewById(R.id.hidden_challenge_recyclerView)
+        val hiddenChallengeRecyclerView: RecyclerView =
+            challengeView.findViewById(R.id.hidden_challenge_recyclerView)
         hiddenChallengeRecyclerView.isNestedScrollingEnabled = false
 
         val hiddenChallengeTitle: TextView = challengeView.findViewById(R.id.hidden_challenge_title)
         val hiddenChallengeText: TextView = challengeView.findViewById(R.id.hidden_challenge_text)
 
         val petChallengeLayoutManager = GridLayoutManager(challengeView.context, 2)
-        val petChallengeRecyclerView: RecyclerView = challengeView.findViewById(R.id.pet_challenge_recyclerView)
+        val petChallengeRecyclerView: RecyclerView =
+            challengeView.findViewById(R.id.pet_challenge_recyclerView)
 
         val petChallengeTitle: TextView = challengeView.findViewById(R.id.pet_challenge_title)
         val petChallengeText: TextView = challengeView.findViewById(R.id.pet_challenge_text)
@@ -63,6 +81,15 @@ class ChallengeFragment : Fragment() {
         val challengeAdapter = DDUBUCKChallengeAdapter()
         challengeRecyclerView.adapter = challengeAdapter
 
+        challengeAdapter.setItemClickListener(object : DDUBUCKChallengeAdapter.ItemClickListener {
+            @RequiresApi(Build.VERSION_CODES.Q)
+            override fun onClick(view: View, position: Int) {
+                toDetailPage(list[position])
+                Log.e("눌림!","눌눌리")
+            }
+        })
+
+
         val hiddenChallengeAdapter = HiddenChallengeAdapter()
         hiddenChallengeRecyclerView.adapter = hiddenChallengeAdapter
 
@@ -72,4 +99,16 @@ class ChallengeFragment : Fragment() {
 
         return challengeView
     }
+
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun toDetailPage(fragment: Fragment) {
+        val fragmentTransaction = parentFragmentManager.beginTransaction()
+        fragmentTransaction
+            .replace(R.id.challenge_fragment, fragment)
+            .addToBackStack(MainActivity.CHALLENGE_TAG)
+            .commit()
+
+    }
+
 }
