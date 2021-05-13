386616c006b235ccac2d86e987735fcac91595b6
exception <suebin710@gmail.com>
2021-05-13 15:27:13 +0900

    add challenge detail adapter

---
+++ DDUBUCK/app/src/main/java/com/example/ddubuck/ui/challenge/detail/ChallengeDetailAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.ddubuck.ui.challenge.detail
+
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ddubuck.MainActivity
+import com.example.ddubuck.R
+import com.example.ddubuck.ui.challenge.HiddenChallengeAdapter
+import kotlinx.android.synthetic.main.challenge_card_layout.view.*
+import kotlinx.android.synthetic.main.challenge_detail_layout.view.*
+
+class ChallengeDetailAdapter() :
+    RecyclerView.Adapter<ChallengeDetailAdapter.ChallengeDetailViewHolder>() {
+
+    val challengeDistanceDetailImages = intArrayOf(
+        R.drawable.ic_place,
+        R.drawable.ic_weather,
+    )
+
+    val hiddenChallengeTitles = arrayOf(
+        "플레이스",
+        "날씨"
+    )
+
+    class ChallengeDetailViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        var challengeDetailItemImage: ImageView = itemView.challenge_detail_image
+        var challengeDetailItemTitle: TextView = itemView.challenge_detail_title
+
+    }
+
+    override fun onCreateViewHolder(
+        viewGroup: ViewGroup,
+        viewType: Int,
+    ): ChallengeDetailViewHolder {
+        val challengeDetailView: View = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.challenge_detail_layout, viewGroup, false)
+
+        return ChallengeDetailViewHolder(challengeDetailView)
+    }
+
+    override fun onBindViewHolder(
+        holder: ChallengeDetailViewHolder,
+        position: Int,
+    ) {
+        holder.challengeDetailItemImage.setImageResource(challengeDistanceDetailImages[position])
+        holder.challengeDetailItemTitle.text = hiddenChallengeTitles[position]
+    }
+
+    override fun getItemCount(): Int {
+        return challengeDistanceDetailImages.size
+    }
+
+
+}
