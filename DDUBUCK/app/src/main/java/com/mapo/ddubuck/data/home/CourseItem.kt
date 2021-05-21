1e65a59e9cc3e2aaab82634dfa3ac3eaf702396d
zeratulspc <zeratulspc@gmail.com>
2021-05-21 15:57:36 +0900

    add radius to dialog imageview

--- DDUBUCK/app/src/main/java/com/mapo/ddubuck/data/home/CourseItem.kt
+++ DDUBUCK/app/src/main/java/com/mapo/ddubuck/data/home/CourseItem.kt
@@ -1,9 +1,5 @@
 package com.mapo.ddubuck.data.home
 
-import android.net.Uri
-import com.google.gson.Gson
-import org.json.JSONObject
-
 data class CourseItem(
     val isFreeWalk : Boolean,
     val imgFile:String?,
@@ -13,9 +9,5 @@ data class CourseItem(
 
     fun compareTo(i : CourseItem) : Boolean {
         return title == i.title
-    }
-
-    fun toJsonString() : String {
-        return Gson().toJson(this@CourseItem)
     }
 }
